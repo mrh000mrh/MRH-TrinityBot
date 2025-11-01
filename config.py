5f321243cacc838e42fcbd1d06c19fb947ffa157
mrh000mrh <82106801+mrh000mrh@users.noreply.github.com>
2025-11-01 22:28:09 +0530

    Add configuration settings for bot and database

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+BOT_TOKEN = "توکن-ربات-شما-اینجا-قرار-بگیرد"
+ADMIN_ID = 123456789  # آی‌دی عددی شما
+
+# تنظیمات پرداخت
+LAQIRA_API_KEY = "your_laqira_api_key_here"
+
+# تنظیمات دیتابیس
+DATABASE_NAME = "database.db"
